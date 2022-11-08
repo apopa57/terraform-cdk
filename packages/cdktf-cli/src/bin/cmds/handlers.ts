641fa520c3d270058488e992184007eb36cd66f1
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-11-08 12:27:22 +0100

    chore: Add ability to get single version package info

--- packages/cdktf-cli/src/bin/cmds/handlers.ts
+++ packages/cdktf-cli/src/bin/cmds/handlers.ts
@@ -484,7 +484,7 @@ export async function providerUpgrade(argv: any) {
   }
 }
 
-export async function providerList() {
+export async function providerList(argv: any) {
   const config = CdktfConfig.read();
   const language = config.language;
   const cdktfVersion = await getPackageVersion(language, "cdktf");
@@ -500,5 +500,9 @@ export async function providerList() {
     config.projectDirectory
   );
 
-  console.log("Todo");
+  const allProviders = await manager.allProviders();
+
+  if (argv.json) {
+    console.log(JSON.stringify(allProviders));
+  }
 }
